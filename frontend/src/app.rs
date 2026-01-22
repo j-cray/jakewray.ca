7c07a864e6bf82ab3209eaaf41e08ec4b61fefd2
Jake Wray <jakewray@mailbox.org>
2026-01-21 19:11:28 -0800

    fix: Rearrange Meta tags in App component for improved SEO and structure

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -34,11 +34,11 @@ pub fn App() -> impl IntoView {
         <html lang="en">
             <head>
                 <Meta charset="utf-8"/>
-                <Title text="Jake Wray"/>
-                <Meta name="description" content="Journalist, Programmer, Photographer."/>
                 <Meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <Stylesheet id="leptos" href="/pkg/jakewray_ca.css"/>
+                <Meta name="description" content="Journalist, Programmer, Photographer."/>
+                <Title text="Jake Wray"/>
                 <MetaTags/>
+                <Stylesheet id="leptos" href="/pkg/jakewray_ca.css"/>
             </head>
 
             <body>
